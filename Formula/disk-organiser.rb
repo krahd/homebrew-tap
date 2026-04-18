4178e2f8d64774a164e3fd46fcce4e282c713ed2
krahd <tomas@laurenzo.net>
2026-04-18 17:43:00 -0600

    disk-organiser: point formula to v0.1.1 macOS binary

--- Formula/disk-organiser.rb
+++ Formula/disk-organiser.rb
@@ -1,8 +1,8 @@
 class DiskOrganiser < Formula
   desc "Visualise and safely organise files on your hard drive"
   homepage "https://github.com/krahd/disk_organiser"
-  url "https://github.com/krahd/disk_organiser/releases/download/v0.1.1/disk-organiser-v0.1.1-macos-installer.tar.gz"
-  sha256 "74043a3d1652be9ac4464ca48bd9bc6b539bd8dbc3c9b80c69c58830a83ccae9"
+  url "https://github.com/krahd/disk_organiser/releases/download/v0.1.1/disk-organiser-v0.1.1-macos-arm64-bin.tar.gz"
+  sha256 "8cec1e55cc202c739b58a38100a5f53b971bf3db60be2a01bed3d3f9544bca70"
   license "MIT"
 
   depends_on "python@3.12"
@@ -11,7 +11,7 @@ class DiskOrganiser < Formula
     libexec.install Dir["*"]
     (bin/"disk-organiser").write <<~EOS
       #!/bin/bash
-      exec "#{Formula[\"python@3.12\"].opt_bin}/python3" "#{libexec}/backend/app.py" "$@"
+      exec "#{Formula[\"python@3.12\"].opt_bin}/python3" "#{libexec}/backend/app.py" ""
     EOS
     chmod 0755, bin/"disk-organiser"
   end
